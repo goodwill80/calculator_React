986981ddc141dfa8343ca9295b47bf54901bb228
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2022-04-30 14:39:46 +0800

    1st commit

---
+++ src/calculator-component.jsx
@@ -0,0 +1,82 @@
+import { useState, useReducer } from 'react';
+import { CalculatorReducer } from './reducer';
+
+const startingState = {
+    currentOperand: '',
+    prevOperand: '',
+    operation: '',
+    overwrite: false
+}
+
+function Calculator() {
+    const btnStyle = "bg-blue-500 w-24 h-24 text-white border border-blue-200 hover:bg-blue-900 rounded-lg text-2xl p-4"
+    const [{ currentOperand, prevOperand, operation }, dispatch] = useReducer(CalculatorReducer, startingState)
+
+    const handleClick = (e)=> {
+        dispatch({ type: "ADD_DIGIT" , digit: e.target.innerText });
+    }
+
+    const handleClear = ()=>{
+        dispatch({ type: "CLEAR" });
+    }
+
+    const handleOperator = (e)=>{
+        dispatch({type: "CHOOSE_OPERAND", operation: e.target.innerText})
+    }
+
+    const handleDelete = ()=>{
+        dispatch({ type: "DELETE" })
+    }
+
+    const handleEvaluate = ()=>{
+        dispatch({ type: "EVALUATE"});
+    }
+    
+
+  return (
+    <div className="flex flex-col justify-center items-center w-1/3 m-auto">
+
+        <div className='relative bg-black h-28 text-white flex flex-col justify-around items-end text-lg rounded-lg' style={{width: "405px"}}>
+            <div className="mr-4">
+                { prevOperand } { operation }
+            </div>
+            <div className="mr-4 text-5xl">
+                { currentOperand }
+            </div>
+            <div className="absolute left-0 top-0 p-4 text-sm text-yellow-200">
+                Casio Fx-123
+            </div>
+          
+        </div>
+        <div className="grid grid-cols-4 gap-2 mt-2">
+        <button className={btnStyle} onClick={handleClick}>7</button>
+        <button className={btnStyle} onClick={handleClick}>8</button>
+        <button className={btnStyle} onClick={handleClick}>9</button>
+        <button className={btnStyle} onClick={handleOperator}>/</button>
+        <button className={btnStyle} onClick={handleClick}>4</button>
+        <button className={btnStyle} onClick={handleClick}>5</button>
+        <button className={btnStyle} onClick={handleClick}>6</button>
+        <button className={btnStyle} onClick={handleOperator}>x</button>
+        <button className={btnStyle} onClick={handleClick}>1</button>
+        <button className={btnStyle} onClick={handleClick}>2</button>
+        <button className={btnStyle} onClick={handleClick}>3</button>
+        <button className={btnStyle} onClick={handleOperator}>-</button>
+        <button className={btnStyle} onClick={handleClick}>0</button>
+        <button className={btnStyle} onClick={handleClick}>.</button>
+        <button className={btnStyle} onClick={handleOperator}>+</button>
+        <button className={btnStyle} onClick={handleEvaluate}>=</button>
+        <button className="bg-blue-500 w-24 h-24 text-white border border-blue-200 hover:bg-blue-900 rounded-lg text-2xl p-4"
+        onClick={handleClear}>Clear</button>
+        <button className="bg-blue-500 w-24 h-24 text-white border border-blue-200 hover:bg-blue-900 rounded-lg text-2xl p-4"
+        onClick={handleDelete}>Delete</button>
+   
+        </div>
+    </div>
+  )
+}
+
+export default Calculator;
+
+
+
+// 
---
+++ src/reducer.jsx
@@ -0,0 +1,82 @@
+const evaluate = ({ currentOperand, prevOperand, operation})=>{
+    const prev = parseFloat(prevOperand);
+    const current = parseFloat(currentOperand);
+    if(isNaN(prev) && isNaN(current)) return '';
+    let computation ='';
+    switch(operation) {
+        case "+":
+            computation = prev + current;
+            break
+        case "-":
+            computation = prev - current;
+            break
+        case "x":
+            computation = prev * current;
+            break
+        case "/":
+            computation = prev / current;
+            break
+    }
+    return computation.toString();
+}
+
+export const CalculatorReducer = (state, action)=>{
+    switch(action.type) {
+        case "ADD_DIGIT":
+            if( action.digit === '0' && state.currentOperand === '0') return state;
+            if( action.digit === '.' && state.currentOperand.includes('.') ) return state;
+            if( state.overwrite) {
+                return {
+                    ...state,
+                    currentOperand: action.digit,
+                    overwrite: false
+                }
+            }
+            return {
+                ...state,
+                currentOperand: `${state.currentOperand}${action.digit}`
+            }
+        case "CLEAR":
+            return { currentOperand: '', prevOperand: '', operation: '' }
+        case "CHOOSE_OPERAND":
+            if( state.currentOperand === "" && state.prevOperand === "") return state;
+            if( state.currentOperand === "") {
+                return {
+                    ...state,
+                    operation: action.operation
+                }
+            }
+            if ( state.prevOperand === "" ) {
+                 return {
+                     ...state,
+                     operation: action.operation,
+                     currentOperand: "",
+                     prevOperand: state.currentOperand
+                 }
+            }
+            return {
+                ...state,
+                operation: action.operation,
+                prevOperand: evaluate(state),
+                currentOperand: ''
+            }
+        case "DELETE":
+            if(state.currentOperand = "") return state;
+            return {
+                ...state,
+                currentOperand: state.currentOperand
+            }
+        case "EVALUATE":
+            if(state.operation === "" || state.currentOperand ==="" || state.prevOperand ===""){
+                return state
+            }
+            return {
+                ...state,
+                operation: "",
+                prevOperand: "",
+                currentOperand: evaluate(state),
+                overwrite: true
+            }
+        default: return state;
+    }
+}
